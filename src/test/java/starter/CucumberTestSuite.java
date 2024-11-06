44cac1a16ffbe4dcfb369c7ca43b28a9a0c1964d
rperdigon <rperdigon@triskellsoftware.com>
2024-11-06 11:36:38 +0100

    added Jenkinsfile

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = {"src/test/resources/features"},
-        tags = "@PROD or @AWS"
+        tags = "@PROD or @AWS",
+        glue = "starter"
 )
 public class CucumberTestSuite {
 }
